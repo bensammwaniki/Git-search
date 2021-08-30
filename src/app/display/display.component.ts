328e8e64f7cba9f1c42c3f94a8b116344954bcbd
bensammwaniki <bensammwaniki@gmail.com>
2021-08-30 03:40:10 +0300

    update constractor methods

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -23,7 +23,14 @@ export class DisplayComponent implements OnInit {
 
 
   constructor( private service: DisplayServService) { 
-
+    this.service.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.service.getProfileRepo().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
   }
 
 
